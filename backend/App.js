c6d52abce28c847b47a16db79fece22815a6b851
GOWTHAM S <gowthamselvaraj327@gmail.com>
2023-07-07 22:48:31 +0530

    Update App.js

--- backend/App.js
+++ backend/App.js
@@ -24,9 +24,9 @@ app.use('/api/v1/',order);
 app.use('/api/v1/',payment);
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,'../frontend/build')));
+    app.use(express.static(path.join(__dirname,'./build')));
     app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
+        res.sendFile(path.resolve(__dirname,'./build/index.html'))
     })
 }
 
